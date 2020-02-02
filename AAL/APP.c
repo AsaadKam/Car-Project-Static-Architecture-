07f81e9034ca3fa4555c8f8943f3f91cfe4b7f26
Mohamed <mohamed.hesh.asad@gmail.com>
2020-02-02 05:37:35 +0200

    Make the state machine

--- AAL/APP.c
+++ AAL/APP.c
@@ -1,134 +1,83 @@
 /* 
- *Software PWM using CTC.c
- *
+ *APP.c
+ *(STATE MACHINE)
  * Created: 06/01/2020 13:29:26
  *  Author: Native_programmer
   */
+#include"Car_Steering.h"
 
-/*-  Defination of Vectors----------------------------------------*/
-
-#define TIMER0_COMP_VECTOR		__vector_10
-#define TIMER1_COMPA_VECTOR  	__vector_7
-#define TIMER1_COMPB_VECTOR 	__vector_8
-#define TIMER2_COMP_VECTOR      __vector_4
-#define TIMER2_OVF			    __vector_5
-
-/*-  Definition of ISR -------------------------------------------*/
-
-#define Timer_ISR(N)            \
-void N (void)__attribute((signal,used)); \
-void N (void)
-
-#include "Atmega32Registers.h"
-#include "Timer.h"
+#define Car_State_Check_Distance					  0
+#define Car_State_Distance_More_than_50               1
+#define Car_State_Distance_Less_than_50_More_than_30  2
+#define Car_State_Distance_Equal_30                   3
+#define Car_State_Distance_Less_than_30               4
 
 int main()
 {
-	/*Initiate Output compare value*/
-	OCR1A=127;
-	/*Initialize Timer 1 as CTC mode and choose 1024 prescaler value*/
-	TCCR1A=0b00000000U;
-	TCCR1B=0b00001101;
-	/*Enable interrupt*/	
-	SREG=1<<7;
-	/*Initialize pin of the Led in portD*/		
-	DDRD|=1<<5;
-	/*Enable timer 1 interrupt*/		
-	TIMSK=1<<OCIE1A;
+	
+    uinteg8_t u8_Car_State=Car_State_Check_Distance;
+	uinteg8_t u8_Measured_Distance;
+	/*Car initialization,includes DC motor driver and ultrasonic sensor*/
+    Car_Init();
 	
 	while(1)
 	{
-       /*Do Nothing ,except prevent code from reach the end*/
+		/*State Machine of car*/
+		switch(u8_Car_State)
+		{
+		   /*Car check distance*/
+			case Car_State_Check_Distance:
+			{
+				Car_Seen_Distance(&u8_Measured_Distance);
+				   
+				if(u8_Measured_Distance>DISTANCE_50_CM)
+				{
+					u8_Car_State=Car_State_Distance_More_than_50;
+				}
+				else if((u8_Measured_Distance<DISTANCE_50_CM)&&(u8_Measured_Distance>DISTANCE_30_CM))
+				{
+					u8_Car_State=Car_State_Distance_Less_than_50_More_than_30;					   
+				}
+				else if(u8_Measured_Distance==DISTANCE_30_CM)
+				{
+					u8_Car_State=Car_State_Distance_Equal_30;
+				}	
+				else if(u8_Measured_Distance<DISTANCE_30_CM)
+				{
+					u8_Car_State=Car_State_Distance_Less_than_30;
+				}				   			   
+			}
+			break;
+		   /*Car move forward 100 percentage of speed in case of the distance more than 50*/			
+			case Car_State_Distance_More_than_50:
+			{
+				Car_Forward(SPEED_RATIO_100);
+				u8_Car_State=Car_State_Check_Distance;
+			}
+			break;
+		   /*Car move forward 50 percentage of speed in case of the distance less than 50 and more than 30*/			
+			case Car_State_Distance_Less_than_50_More_than_30 :
+			{
+				Car_Forward(SPEED_RATIO_50);
+				u8_Car_State=Car_State_Check_Distance;																				
+			}
+			break;
+		   /*Car move rotate 90 degree in case of the distance equal 30 */							
+			case Car_State_Distance_Equal_30 :
+			{
+				Car_Rotate_90_Degree();
+				u8_Car_State=Car_State_Check_Distance;										
+			}
+			break;
+		   /*Car move backward 15 percentage of speed in case of the distance ;ess than  30 */						
+			case Car_State_Distance_Less_than_30 :
+			{
+				Car_Backward(SPEED_RATIO_15);		
+				u8_Car_State=Car_State_Check_Distance;								
+			}
+			break;			
+						
+		}
 	}
 	
 }
-/*ISR implementation*/
-Timer_ISR(TIMER1_COMPA_VECTOR)
-{
-    /*State variable*/
-	static uinteg8_t u8_state=0;
-	
-	switch(u8_state)
-	{
-		/*In case number one we are going to  set the 1st value to get the software PWM */
-		case 0:
-		{
-			/**Setting the 1nd value to get the relevant duty cycle*/			
-			OCR1A=127;
-			/**Toggle LED on port D pin 5*/
-			PORTD^=(1<<5);
-			/*Change the state*/
-			u8_state=1;
-		}
-		break;
-		/*In case number two we are going to set the 2nd value  to get the software PWM */		
-		case 1 :
-		{
-			/**Setting the 2nd value to get the relevant duty cycle*/			
-			OCR1A=128;
-			/**Toggle LED on port D pin 5*/			
-			PORTD^=(1<<5);
-			/*Change the state*/			
-			u8_state=0;
-		}
-		break;
-	}
-
-}
-
-/*
-
-
-
-int main()
-{
-	/*Initiate Output compare value*/	
-	OCR2=127;
-	/*Initialize Timer 2 as CTC mode and choose 1024 prescaler value*/	
-	TCCR2=0B00001111;
-	/*Enable interrupt*/	
-	SREG=1<<7;
-	/*Initialize pin of the Led in portD*/		
-	DDRD|=1<<5;
-	/*Enable timer 2 interrupt*/			
-	TIMSK=1<<OCIE2;
-	while(1)
-	{
-
-	}
-	
-}
-/*ISR implementation*/
-Timer_ISR(TIMER2_COMP_VECTOR)
-{
-    /*State variable*/
-	static uinteg8_t u8_state=0;
-	
-	switch(u8_state)
-	{
-		/*In case number one we are going to  set the 1st value to get the software PWM */		
-		case 0:
-		{
-			/**Setting the 1nd value to get the relevant duty cycle*/			
-			OCR2=127;
-			/**Toggle LED on port D pin 5*/			
-			PORTD^=(1<<5);
-			/*Change the state*/			
-			u8_state=1;
-		}
-		break;
-		/*In case number two we are going to set the 2nd value  to get the software PWM */			
-		case 1 :
-		{
-			/**Setting the 2nd value to get the relevant duty cycle*/			
-			OCR2=128;
-			/**Toggle LED on port D pin 5*/				
-			PORTD^=(1<<5);
-			/*Change the state*/			
-			u8_state=0;
-		}
-		break;
-	}
-
-}
-*/
--- Debug/AAL/APP.d
+++ Debug/AAL/APP.d
@@ -1,13 +1,50 @@
-AAL/APP.d AAL/APP.o: ../AAL/APP.c ../Headers/Atmega32Registers.h \
- ../Headers/Data_types.h ../Headers/Timer.h ../Headers/Timer.h \
- ../Headers/Data_Types.h
+AAL/APP.d AAL/APP.o: ../AAL/APP.c ../Headers/Car_Steering.h \
+ ../Headers/Delay_without_timer.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ ../Headers/DC_Motor_Driver.h ../Headers/DC_Motor_Driver_User_Interface.h \
+ ../Headers/DC_Motor_Driver_PostBConfig.h ../Headers/Timer.h \
+ ../Headers/Data_Types.h ../Headers/DC_Motor_Driver_Designer.h \
+ ../Headers/Ultrasonic.h ../Headers/Ultrasonic_Designer.h \
+ ../Headers/Car_Steering_Designer.h \
+ ../Headers/Car_Steering_User_Interface.h
 
-../Headers/Atmega32Registers.h:
+../Headers/Car_Steering.h:
 
-../Headers/Data_types.h:
+../Headers/Delay_without_timer.h:
 
-../Headers/Timer.h:
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+../Headers/DC_Motor_Driver.h:
+
+../Headers/DC_Motor_Driver_User_Interface.h:
+
+../Headers/DC_Motor_Driver_PostBConfig.h:
 
 ../Headers/Timer.h:
 
 ../Headers/Data_Types.h:
+
+../Headers/DC_Motor_Driver_Designer.h:
+
+../Headers/Ultrasonic.h:
+
+../Headers/Ultrasonic_Designer.h:
+
+../Headers/Car_Steering_Designer.h:
+
+../Headers/Car_Steering_User_Interface.h:
